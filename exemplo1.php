63e0e4825bbd64a2e1398f8e6e743af5555fb97c
emderma12 <93205579+emderma12@users.noreply.github.com>
2021-12-15 09:58:45 -0300

    envio exemplos jason decode

---
+++ exemplo1.php
@@ -0,0 +1,13 @@
+<?php 
+
+$json_str = '{"nome":"jason jones", "idade":38, "sexo": "M"}';
+
+$obj = json_decode($json_str);
+
+echo "nome: $obj->nome<br>";
+echo "idade: $obj->idade<br>";
+echo "sexo: $obj->sexo<br>";
+
+
+
+ ?>
